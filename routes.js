84a65e6a52c1604cb887a18311987b716da31162
Remigiusz Kolanko <remigiusz.kolanko@synergycodes.com>
2020-02-21 08:20:17 +0100

    initial commit

---
+++ routes.js
@@ -0,0 +1,38 @@
+const fs = require('fs');
+
+const requestHandler = (req, res) => {
+    const url = req.url;
+    const method = req.method;
+
+    if(url === '/') {
+        res.write('<html>');
+        res.write('<head><title>Enter message</title></head>');
+        res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
+        res.write('</html>');
+        return res.end();
+    }
+    
+    if(url === '/message' && method === 'POST') {
+        const body = [];
+        req.on('data', (chunk) => {
+            body.push(chunk);
+        });
+        return req.on('end', () => {
+            const parsedBody = Buffer.concat(body).toString();
+            const message = parsedBody.split('=')[1];
+            fs.writeFile('message.txt', message, (err) => {
+                res.statusCode = 302;
+                res.setHeader('Location', '/');
+                return res.end();
+            });
+        });
+    }
+    
+    res.write('<html>');
+    res.write('<head><title>First Page</title></head>');
+    res.write('<body><p>Node page</p></body>');
+    res.write('</html>');
+};
+
+exports.handler = requestHandler;
+exports.someText = 'Some hard coded text';
